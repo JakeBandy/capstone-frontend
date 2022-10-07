6304ce3ae14650a1c6d68c981e29550b625feeb9
JakeBandy <jakebandy13@hotmail.com>
2022-10-06 20:59:40 -0500

    repo issues

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { CreateUserForm } from "./CreateUserForm";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodoData = async () => {
+      const data = await fetch("capstone-backend-ten.vercel.app/todos");
+
+      const json = await data.json();
+
+      setTodos(json);
+    };
+
+    fetchTodoData();
+  }, []);
+
+  useEffect(() => {
+    console.log({ todos });
+  }, [todos]);
+
+  return (
+    <div className="App">
+
+    <h1>Welcome, Jake</h1>
+    <h4>Your tasks</h4>
+    <CreateUserForm />
+    <h1>{ todos }</h1>
+    <h2>-Eat breakfast</h2>
+    <h2>-Go to the gym</h2>
+    <h2>-Do coding interviews</h2>
+
+  </div>
+  );
+}
+
+export default App;
